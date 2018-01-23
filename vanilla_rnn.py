81371e98d58b0b8b01133d043e2389c808dec8e9
chosenone75 <lebronran@gmail.com>
2018-01-23 22:43:40 +0800

    init commit

---
+++ vanilla_rnn.py
@@ -0,0 +1,10 @@
+import numpy as np
+import logging
+import matplotlib.pyplot as plt
+
+class BasicRNN(object):
+    """docstring for BasicRNN"""
+    def __init__(self, arg):
+        super(BasicRNN, self).__init__()
+        self.arg = arg
+
